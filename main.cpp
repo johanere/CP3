33bbeb365c20834b29e609bbc429e5af64035024
johanere <42669090+johanere@users.noreply.github.com>
2018-10-18 19:30:43 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -15,24 +15,34 @@ int main()
 
     // Specify discretization paramters through argsys
     //hent fra argsys <- FIKS
-    int N = 100;
+    int N = 1000;
 
 
     // Initializes sun, earth
     double UnitMass=1988500e24; //kg
+
+    double veladjust=365.242199;
 
     double massSun = 1; //
     vec3 posSun(0,0,0);
     vec3 velSun(0,0,0);
 
     double massEarth = 5.97219e24/UnitMass; // massEarth / UnitMass
-    vec3 posEarth(1,0,0);
-    vec3 velEarth(0,2*M_PI,0); // v=2pi r/period -> r=1 AU, period=1 yr (standard units)
+    vec3 posEarth(9.079361611992910E-01,4.174196284373897E-01,-9.320364656857048E-05);
+    vec3 velEarth(-7.374461784272602E-03,1.561335766240027E-02,1.196212616732829E-07); // v=2pi r/period -> r=1 AU, period=1 yr (standard units)
+
+
+    double massMars = 6.6e23/UnitMass; // massEarth / UnitMass
+    vec3 posMars(1.384662720209662E+00,-8.711968326251433E-02,-3.603051751471911E-02);
+    vec3 velMars(1.478464132931038E-03,1.515952151725583E-02,2.813270986502590E-04); // v=2pi r/period -> r=1 AU, period=1 yr (standard units)
+
+    velMars=velMars*veladjust;
+    velEarth=velEarth*veladjust;
 
     //initialize objects
     CelestialBody *sun = new CelestialBody(posSun, velSun, massSun, "sun");
     CelestialBody *earth = new CelestialBody(posEarth, velEarth, massEarth, "earth");
-
+    CelestialBody *mars = new CelestialBody(posMars, velMars, massMars, "mars");
     double G = 4*M_PI*M_PI;
 
     System SunEarth;  //construct system
@@ -43,7 +53,7 @@ int main()
     //add objects to system class
     SunEarth.addObject(sun);
     SunEarth.addObject(earth);
-
+    SunEarth.addObject(mars);
 
 
     Solver Sol(SunEarth,force);
---
+++ plotmultibody.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import string as str
+outdata=["3_data_200.txt"]
+
+planets=["Sun","Earth","Mars"]
+
+for j in range(len(outdata)):  
+    data=np.loadtxt(outdata[j],unpack=True)
+    n=int(data[0,0])
+    m=int(data[0,1])
+    
+    pos=np.zeros((n,m))
+    vel=np.zeros((n,m))
+
+
+    pos=np.asarray(data[:,2::2])
+    vel=np.asarray(data[:,3::2])
+    
+    xpos=pos[0]
+    ypos=pos[1]
+    zpos=pos[2]
+
+    xvel=vel[0]
+    yvel=vel[1]
+    zvel=vel[2]
+
+    print m
+    print xpos[0:6]
+ 
+    for i in range(0,m):
+        plt.plot(xpos[i::m],ypos[i::m],label='%s'%planets[i])
+   
+       
+plt.xlabel('$x[AU]$')
+plt.ylabel('$y[AU]$')
+plt.grid('on')
+plt.title('The Solar system with %s bodies'%m)
+plt.legend(loc=0)
+plt.show()
